0f1338415ab39080297982aecc0bcbaac70bfb38
quanvm <vuminhquan2904@gmail.com>
2025-10-15 23:08:01 +0700

    update code

--- src/main/java/com/quanvm/applyin/controller/UploadController.java
+++ src/main/java/com/quanvm/applyin/controller/UploadController.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/upload")
+@RequestMapping("/api/upload")
 public class UploadController {
 
   private final S3Client s3Client;
